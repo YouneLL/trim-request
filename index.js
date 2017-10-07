821ccd147d790c56ba68f62495354464721f05be
Yns <yns@nunu.com>
2017-10-07 17:51:04 +0100

    remove hasOwnProperty function because request object  does not prototypically extend from the JavaScript Object

--- index.js
+++ index.js
@@ -5,19 +5,14 @@ function trimStringProperties (obj) {
 
         for ( var prop in obj ) {
 
-            // exclude properties in the prototype
-            if ( obj.hasOwnProperty(prop) ) {
-
-                // if the property is an object trim it too
-                if ( typeof obj[prop] === 'object' ) {
-                    return trimStringProperties(obj[prop]);
-                } 
-                
-                // if it's a string remove begin and end whitespaces
-                if ( typeof obj[prop] === 'string' ) {
-                    obj[prop] = obj[prop].trim();
-                }
-
+            // if the property is an object trim it too
+            if ( typeof obj[prop] === 'object' ) {
+                return trimStringProperties(obj[prop]);
+            } 
+            
+            // if it's a string remove begin and end whitespaces
+            if ( typeof obj[prop] === 'string' ) {
+                obj[prop] = obj[prop].trim();
             }
 
         }
